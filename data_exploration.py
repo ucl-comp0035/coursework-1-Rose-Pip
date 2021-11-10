8731d5feed1b8f240b31b834c1ee2d526c2b58b3
Rose-Pip <92257712+Rose-Pip@users.noreply.github.com>
2021-11-10 15:25:13 +0000

    data preparation for 2021 and 2018

--- data_exploration.py
+++ data_exploration.py
@@ -1,1 +1,18 @@
 # Write code that explores your data set
+import pandas as pd 
+import numpy as np 
+if __name__=='__main__':
+    '''
+    df_2021 is the recorded data in 2021 
+    df_2018 is the recorded data in 2018
+    '''
+    df_2021=pd.read_csv(r'raw data/Borough_tree_list_2021.csv', low_memory=False)
+    display(df_2021) # to visualise the raw table of trees ditribution recorded in 2021 
+    df_cl_21=df_2021.drop(columns=['taxon_name','lon','lat','load_date','objectid','gdb_geomattr_data','diameter_at_breast_height_cm'])
+    display(df_cl_21)
+    
+    df_2018=pd.read_excle(r'raw data/london_street_trees_gla_20180214.xlsx')
+    display(df_2018) # to visualise the raw table of trees distribution in 2018
+    df_cl_18=df_2018.drop(columns=['longtitude','latitude','easting','northing','longitude','latitude'])
+    df_pre_21=df_cl_21.to_csv(r'C:\Users\yis27\Autodesk\Downloads\Borough_tree_list_2021_cleaned.csv')
+    
